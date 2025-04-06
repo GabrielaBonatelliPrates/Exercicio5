5432187f9580bfad3eb5ebb53985973a487ff897
rafae <rafae@RaFeIGa>
2025-04-06 14:35:59 -0300

    inicio do projeto

---
+++ src/main/java/com/mycompany/exercicio5/Exercicio5.java
@@ -0,0 +1,17 @@
+
+
+package com.mycompany.exercicio5;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rafae
+ */
+public class Exercicio5 {
+
+    public static void main(String[] args) {
+       int numero = 6;
+       JOptionPane.showMessageDialog(null, numero);
+    }
+}
